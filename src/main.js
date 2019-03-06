eef94f7846eecbb9dd1ba627e260cad503dee5c7
Marine Teissier <marine.teissier@groupedubreuil.com>
2019-03-06 15:51:20 +0100

    init bootstrap vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+// add bootstrap vue + bootstrap 4
+import '@/assets/css/custom.scss'
+import { Layout } from 'bootstrap-components'
+Vue.use(Layout)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
